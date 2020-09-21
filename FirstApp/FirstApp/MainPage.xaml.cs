8b47bb57082fee704b0b1a291caeeb0a042d0a41
Flakes <ed.afram@gmail.com>
2020-09-21 00:41:52 -0300

    Aplicativo em andamento.

--- FirstApp/FirstApp/MainPage.xaml.cs
+++ FirstApp/FirstApp/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FirstApp.ViewModel;
 using Xamarin.Forms;
 
 namespace FirstApp
@@ -13,6 +8,7 @@ namespace FirstApp
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = new MainPageViewModel();
         }
     }
 }
---
+++ FirstApp/FirstApp/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp.ViewModel
+{
+    public class MainPageViewModel
+    {
+
+    }
+}
