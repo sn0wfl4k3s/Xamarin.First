7ab38330bdfe83ca9062cc4d165c3e3b78c6f536
Flakes <ed.afram@gmail.com>
2020-09-21 14:38:24 -0300

    service changed

--- FirstApp/FirstApp/Services/NoteService.cs
+++ FirstApp/FirstApp/Services/NoteService.cs
@@ -7,6 +7,17 @@ namespace FirstApp.Services
 {
     public class NoteService : IDataStore<Note>
     {
+        private ObservableCollection<Note> _notes;
+
+        public NoteService()
+        {
+            _notes = new ObservableCollection<Note> 
+            {
+                new Note { Title = "titulo 1", Description = "Description número um." },
+                new Note { Title = "titulo 2", Description = "Description número dois." }
+            };
+        }
+
         public Task AddEntity(Note entity)
         {
             throw new NotImplementedException();
@@ -17,14 +28,7 @@ namespace FirstApp.Services
             throw new NotImplementedException();
         }
 
-        public ObservableCollection<Note> GetAllEntities()
-        {
-            return new ObservableCollection<Note>
-            {
-                new Note { Title = "titulo 1", Description = "Description número um." },
-                new Note { Title = "titulo 2", Description = "Description número dois." },
-            };
-        }
+        public ObservableCollection<Note> GetAllEntities() => _notes;
 
         public Task<ObservableCollection<Note>> GetAllEntitiesAsync()
         {
