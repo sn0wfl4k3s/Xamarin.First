2e63a48950847d09f7aa699c33cabce12a80fc13
Flakes <ed.afram@gmail.com>
2020-09-21 01:14:35 -0300

    listview working

---
+++ FirstApp/FirstApp/Models/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstApp.Models
+{
+    public class Note
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
--- FirstApp/FirstApp/ViewModel/MainPageViewModel.cs
+++ FirstApp/FirstApp/ViewModel/MainPageViewModel.cs
@@ -1,7 +1,14 @@
-﻿namespace FirstApp.ViewModel
+﻿using FirstApp.Models;
+using System.Collections.ObjectModel;
+
+namespace FirstApp.ViewModel
 {
     public class MainPageViewModel
     {
-
+        public ObservableCollection<Note> Notes { get; } = new ObservableCollection<Note>
+        {
+            new Note { Title = "titulo 1", Description = "Description número um." },
+            new Note { Title = "titulo 2", Description = "Description número dois." },
+        };
     }
 }
