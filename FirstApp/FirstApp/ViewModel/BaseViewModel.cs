89958930434f84ac0b278a6532d957faa336d122
Flakes <ed.afram@gmail.com>
2020-09-22 22:18:54 -0300

    delete de items funcionando.

--- FirstApp/FirstApp/ViewModel/BaseViewModel.cs
+++ FirstApp/FirstApp/ViewModel/BaseViewModel.cs
@@ -1,11 +1,19 @@
 ﻿using FirstApp.Models;
 using FirstApp.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 
 namespace FirstApp.ViewModel
 {
-    public abstract class BaseViewModel
+    public abstract class BaseViewModel : INotifyPropertyChanged
     {
         protected readonly IDataStore<Note> _dataStore = DependencyService.Get<IDataStore<Note>>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- FirstApp/FirstApp/ViewModel/MainViewModel.cs
+++ FirstApp/FirstApp/ViewModel/MainViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using FirstApp.Models;
-using FirstApp.Services;
 using FirstApp.Views;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
@@ -10,18 +9,22 @@ namespace FirstApp.ViewModel
 {
     public class MainViewModel : BaseViewModel
     {
-        public ICommand OpenCreateNotePageCommand { get; private set; }
-        
         public ObservableCollection<Note> Notes { get; set; }
+
+        public ICommand OpenCreateNotePageCommand => new Command(OpenCreateNotePageCmd);
+        public ICommand DeleteNoteCommand => new Command<Note>(DeleteNoteCmd);
+
 
         public MainViewModel()
         {
             Notes = _dataStore.GetAllEntities();
-
-            OpenCreateNotePageCommand = new Command(OpenCreateNotePageCmd);
         }
 
-        
+        private void DeleteNoteCmd(Note note)
+        {
+            _dataStore.DeleteEntity(note);
+        }
+
         private async void OpenCreateNotePageCmd()
         {
             await Application.Current.MainPage.Navigation.PushAsync(new CreateNotePage());
