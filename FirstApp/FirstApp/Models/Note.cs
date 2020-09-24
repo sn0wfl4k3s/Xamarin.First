1a2420ab4fe40fedf98ec4177cb4674048bc1096
Flakes <ed.afram@gmail.com>
2020-09-23 22:15:10 -0300

    inclusão da lib Fody para fazer o notifyproperties sem precisar implementar.

--- FirstApp/FirstApp/Models/Note.cs
+++ FirstApp/FirstApp/Models/Note.cs
@@ -1,31 +1,12 @@
-﻿using FirstApp.Services;
+﻿using PropertyChanged;
 
 namespace FirstApp.Models
 {
-    public class Note : NotifyPropertiesService
+    [AddINotifyPropertyChangedInterface]
+    public class Note
     {
         public int Id { get; set; }
-
-        private string _title = string.Empty;
-        public string Title 
-        {
-            get { return _title; }
-            set
-            {
-                _title = value;
-                NotifyPropertyChanged();
-            }
-        }
-
-        private string _description = string.Empty;
-        public string Description 
-        { 
-            get { return _description; }
-            set
-            {
-                _description = value;
-                NotifyPropertyChanged();
-            }
-        }
+        public string Title { get; set; }
+        public string Description { get; set; }
     }
 }
