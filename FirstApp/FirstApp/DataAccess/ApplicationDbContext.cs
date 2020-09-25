05f46784dd147764dff4a5b606c0564dccf295b9
Flakes <ed.afram@gmail.com>
2020-09-25 20:38:06 -0300

    inclusão do context.

---
+++ FirstApp/FirstApp/DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using FirstApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+
+using Xamarin.Forms;
+
+namespace FirstApp.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        private readonly string dbFilename = "banco1.db";
+
+        public virtual DbSet<Note> Notes { get; set; }
+
+        public ApplicationDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = string.Format("Data Source={0}", DatabasePath);
+
+            optionsBuilder.UseSqlite(connection);
+        }
+
+        private string DatabasePath => Device.RuntimePlatform switch
+        {
+            Device.Android => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), dbFilename),
+            Device.iOS or Device.macOS => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", dbFilename),
+            Device.UWP => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), dbFilename),
+            _ => throw new Exception("Sistema não suportado")
+        };
+    }
+}
