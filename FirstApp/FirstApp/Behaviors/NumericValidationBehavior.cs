c7909308940f398d0ff3b33ffb74fad0d8682b3d
Flakes <ed.afram@gmail.com>
2020-09-26 19:17:02 -0300

    first behavior

---
+++ FirstApp/FirstApp/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,31 @@
+﻿using Xamarin.Forms;
+
+namespace FirstApp.Behaviors
+{
+    public class NumericValidationBehavior : Behavior<Entry>
+    {
+        public string MessageError { get; set; }
+
+        protected override void OnAttachedTo(Entry bindable)
+        {
+            bindable.TextChanged += OnEntryTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(Entry bindable)
+        {
+            bindable.TextChanged -= OnEntryTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+
+        void OnEntryTextChanged(object sender, TextChangedEventArgs args)
+        {
+            bool isValid = double.TryParse(args.NewTextValue, out _);
+            
+            (sender as Entry).TextColor = isValid ? Color.Default : Color.Red;
+        }
+
+
+    }
+}
